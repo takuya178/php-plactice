f49ca4f0427d9f8297228150cab0b4cbc6a90d80
takuya178 <takuya.u1005@gmail.com>
2021-09-05 15:12:31 +0900

    成分の選択画面

--- app/javascript/packs/home/select.js
+++ app/javascript/packs/home/select.js
@@ -1,48 +1,10 @@
 
 
-  // class Switch {
-  //   constructor(obj) {
-  //     const link = document.querySelector(obj.selectName);
-  //     const text = document.querySelector(obj.tagName);
-  //     const check = document.querySelector(obj.checkName);
-  //     const image = document.querySelector(obj.imageName)
-
-  //     link.addEventListener('click', (e) => {
-  //       this.handleClick(e)
-  //       if(text.style.display == 'block') {
-  //         text.style.display = 'none'
-  //         check.style.display = 'block'
-  //       } else {
-  //         text.style.display = 'block'
-  //         check.style.display = 'none'
-  //       }
-  //       image.classList.toggle('filter');
-  //     });
-  //   }
-
-  //   handleClick = (e) => {
-  //     e.preventDefault();
-  //   }
-  // }
-
-  const $link_button = document.getElementsByTagName('a')
-  
-
-  const sugar_link = $link_button[1];
-  const sugar_text = document.querySelector('#js-sugar-text');
-  const sugar_check = document.querySelector('#js-sugar-check');
-  const sugar_image = document.querySelector('#js-sugar-image');
-
-  const lipid_link = $link_button[2]
-  const lipid_text = document.querySelector('#js-lipid-text');
-  const lipid_check = document.querySelector('#js-lipid-check');
-  const lipid_image = document.querySelector('#js-lipid-image');
-
-  const salt_link = $link_button[3]
-  const salt_text = document.querySelector('#js-salt-text');
-  const salt_check = document.querySelector('#js-salt-check');
-  const salt_image = document.querySelector('#js-salt-image');
-
+  const $tab = document.getElementById('tab')
+  const $link = $tab.querySelectorAll('a')
+  const $img = $tab.querySelectorAll('img')
+  const $text = $tab.querySelectorAll('h2')
+  const $check = $tab.querySelectorAll('h1')
 
   const sugarId = [];
   const lipidId = [];
@@ -56,64 +18,62 @@
 
 
   // 糖質部分がクリックされた処理
-  sugar_link.addEventListener('click', (e) => {
+  $link[0].addEventListener('click', (e) => {
     handleClick(e)
-    if(sugar_text.style.display == 'block') {
-      sugar_text.style.display = 'none'
-      sugar_check.style.display = 'block'
-      sugarId.push(sugar_link.dataset.name)
+    if($text[0].style.display == 'block') {
+      $text[0].style.display = 'none'
+      $check[0].style.display = 'block'
+      sugarId.push($link[0].dataset.id)
     } else {
-      sugar_text.style.display = 'block'
-      sugar_check.style.display = 'none'
-      sugarId.pop(sugar_link.dataset.name)
+      $text[0].style.display = 'block'
+      $check[0].style.display = 'none'
+      sugarId.pop($link[0].dataset.id)
     }
-    sugar_image.classList.toggle('filter');
     console.log(sugarId)
+    $img[0].classList.toggle('filter');
   });
 
   // 脂質部分がクリックされた処理
-  lipid_link.addEventListener('click', (e) => {
+  $link[1].addEventListener('click', (e) => {
     handleClick(e)
-    if(lipid_text.style.display == 'block') {
-      lipid_text.style.display = 'none'
-      lipid_check.style.display = 'block'
-      lipidId.push(lipid_link.dataset.name)
+    if($text[1].style.display == 'block') {
+      $text[1].style.display = 'none'
+      $check[1].style.display = 'block'
+      lipidId.push($link[1].dataset.id)
     } else {
-      lipid_text.style.display = 'block'
-      lipid_check.style.display = 'none'
-      lipidId.pop(lipid_link.dataset.name)
+      $text[1].style.display = 'block'
+      $check[1].style.display = 'none'
+      lipidId.pop($link[1].dataset.id)
     }
-    lipid_image.classList.toggle('filter');
-    console.log(lipidId)
+    $img[1].classList.toggle('filter');
   });
 
   // 塩分がクリックされた処理
-  salt_link.addEventListener('click', (e) => {
+  $link[2].addEventListener('click', (e) => {
     handleClick(e)
-    if(salt_text.style.display == 'block') {
-      salt_text.style.display = 'none'
-      salt_check.style.display = 'block'
-      saltId.push(salt_link.dataset.name)
+    if($text[2].style.display == 'block') {
+      $text[2].style.display = 'none'
+      $check[2].style.display = 'block'
+      saltId.push($link[2].dataset.id)
     } else {
-      salt_text.style.display = 'block'
-      salt_check.style.display = 'none'
-      saltId.pop(salt_link.dataset.name)
+      $text[2].style.display = 'block'
+      $check[2].style.display = 'none'
+      saltId.pop($link[2].dataset.id)
     }
-    salt_image.classList.toggle('filter');
-    console.log(saltId)
+    $img[2].classList.toggle('filter');
   });
 
   // ボタンを押して/resultページへ
   button.addEventListener('click', () => {
     $.ajax({
       type: 'get',
-      url: '/foods',
+      url: '/foods/result',
       data: {
-        sugar_tag: sugarId,
-        lipid_tag: lipidId,
-        salt_tag: saltId, 
+        sugar: sugarId,
+        lipid: lipidId,
+        salt: saltId, 
       }
     }).done(() => {
-      window.location.href = '/foods?sugar%5B%5D=' + sugarId + '&lipid%5B%5D=' + lipidId + '&salt%5B%5D=' + saltId;
+      window.location.href = '/foods/result?sugar%5B%5D=' + sugarId + '&lipid%5B%5D=' + lipidId + '&salt%5B%5D=' + saltId;
     });
   });
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_115628) do
+ActiveRecord::Schema.define(version: 2021_09_04_132734) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2021_08_26_115628) do
     t.float "sugar"
     t.float "lipid"
     t.float "salt"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "component"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
