51e92ee53e99ffb593f981a9b2ac5344fa5092b4
takuya178 <takuya.u1005@gmail.com>
2021-10-14 16:22:18 +0900

    メイン料理編集

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,5 +1,15 @@
 class MainsController < ApplicationController
   def index
-    @mains = Main.all.page(params[:page]).per(10)
+    @q = Main.ransack(params[:q])
+    @mains = @q.result(distinct: true).all.page(params[:page]).per(10)
+    if params[:q].present?
+      @search = Main.ransack(params[:q])
+      @datespots = @search.result
+    else
+    # 検索フォーム以外からアクセスした時の処理（デフォルトの並び順）
+      params[:q] = { sorts: 'date asc' }
+      @search = Main.ransack(params[:q])
+      @datespots = @search.result
+    end
   end
 end
