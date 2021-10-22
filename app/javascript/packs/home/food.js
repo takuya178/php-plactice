5c8788dbe182a1460142cc9618cd31a75d9fd75b
takuya178 <takuya.u1005@gmail.com>
2021-10-22 20:42:43 +0900

    スクロールトップ追加

--- app/javascript/packs/home/food.js
+++ app/javascript/packs/home/food.js
@@ -8,10 +8,40 @@
   const table = document.getElementById('food_table');
   console.log(table)
   const ngfood = document.getElementById('js-ngfood');
+  const scroll = document.getElementById( 'js-scroll-fadein' );
 
 
   ngfood.addEventListener('click', () => {
     location.href = '/overdose_food_combinations' + location.search
   });
 
+
+  // スクロールトップ
+
+  //スクロール量を取得する関数
+  const getScrolled = () => {
+    let ref;
+    if(window.pageYOffset !== undefined) {
+      ref=window.pageYOffset;
+    }else {
+      ref=document.documentElement.scrollTop;
+    }
+    return ref;
+  };
+  //ボタンの表示・非表示
+  window.onscroll = () => {
+    if(getScrolled() > 500) {
+      scroll.classList.add( 'is-fadein' )
+    }else {
+      scroll.classList.remove( 'is-fadein' );
+    }
+  };
+  // トップに移動する
+  const scrollTop = () => {
+    window.scrollTo( 0, 0 );
+  }
+  // スクロールボタンをクリックできるようにする
+  scroll.addEventListener('click', () => {
+    scrollTop();
+  })
 })();
--- app/javascript/packs/home/ng.js
+++ app/javascript/packs/home/ng.js
@@ -1,6 +1,36 @@
 const normalbtn = document.getElementById('js-normalbtn');
+const scroll = document.getElementById( 'js-scroll-fadein' );
 
 normalbtn.addEventListener('click', () => {
   ref = document.referrer
   location.href = ref;
 });
+
+  // スクロールトップ
+
+  //スクロール量を取得する関数
+  const getScrolled = () => {
+    let index;
+    if(window.pageYOffset !== undefined) {
+      index = window.pageYOffset;
+    }else {
+      index = document.documentElement.scrollTop;
+    }
+    return index;
+  };
+  //ボタンの表示・非表示
+  window.onscroll = () => {
+    if(getScrolled() > 500) {
+      scroll.classList.add( 'is-fadein' )
+    }else {
+      scroll.classList.remove( 'is-fadein' );
+    }
+  };
+  // トップに移動する
+  const scrollTop = () => {
+    window.scrollTo( 0, 0 );
+  }
+  // スクロールボタンをクリックできるようにする
+  scroll.addEventListener('click', () => {
+    scrollTop();
+  })
