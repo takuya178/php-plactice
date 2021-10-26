1629165eb56d60341deda3ade98446dc97e64760
takuya178 <takuya.u1005@gmail.com>
2021-10-26 21:46:30 +0900

    使い方説明＆ログイン機能削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-
-  get 'login',  to: 'user_sessions#new'
-  post 'login', to: 'user_sessions#create'
-  delete 'logout', to: 'user_sessions#destroy'
-  post 'guest_login', to: 'user_sessions#guest_login'
   get 'privacy', to: 'static_pages#privacy'
   get 'terms', to: 'static_pages#terms'
   get 'explanation', to: 'static_pages#explanation'
