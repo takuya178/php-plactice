0a19b05a811928132904122246a41f1a21152335
takuya178 <takuya.u1005@gmail.com>
2021-10-29 18:28:20 +0900

    組み合わせ作成ページデザイン変更

--- app/models/main.rb
+++ app/models/main.rb
@@ -4,11 +4,25 @@ class Main < ApplicationRecord
   has_one_attached :image
   accepts_nested_attributes_for :subs, allow_destroy: true
 
-  validates :name, presence: true
-  validates :calorie, presence: true
-  validates :sugar, presence: true
-  validates :lipid, presence: true
-  validates :salt, presence: true
+  include FoodValidate
 
-  enum genre: { noodle: 0, rice: 1, bread: 2, candy: 3 }
+  validate :image_content_type, :image_size, if: :was_attached?
+
+  def image_content_type
+    image_type = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, "の拡張子は.png .jpg .jpegのみです") unless image.content_type.in?(image_type)
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
+
+  def image_size
+      if image.blob.byte_size > 5.megabytes
+        image.purge
+        errors.add(:image, "は5MB以内にしてください")
+      end
+  end
+
+  enum genre: { noodle: 0, rice: 1 }
 end
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -2,4 +2,26 @@ class Sub < ApplicationRecord
   has_many :food_combinations, dependent: :destroy
   has_many :mains, through: :food_combinations
   has_one_attached :image
+
+  include FoodValidate
+
+  validate :image_content_type, :image_size, if: :was_attached?
+
+  def image_content_type
+    image_type = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, "の拡張子は.png .jpg .jpegのみです") unless image.content_type.in?(image_type)
+  end
+
+  def was_attached?
+    self.image.attached?
+  end
+
+  def image_size
+    if image.blob.byte_size > 5.megabytes
+      image.purge
+      errors.add(:image, "は5MB以内にしてください")
+    end
+  end 
+
+
 end
